2be9bc86812c347d9e386b7bb71b1137b245cf5e
leesanghyup <bosuksh@gmail.com>
2023-02-25 17:02:11 +0900

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Template from "./components/Template";
+import TodoList from "./components/TodoList";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할일 1",
+      checked: true
+    },
+    {
+      id: 2,
+      text: "할일 2",
+      checked: false
+    },
+    {
+      id: 3,
+      text: "할일 3",
+      checked: true
+    }
+  ])
+  return <Template><TodoList todos={todos}></TodoList></Template>
 }
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+const TodoList = ({todos}) => {
+    return <div>{todos.map(
+        todo => <TodoItem todo={todo} key={todo.id}/>)
+    }</div>
+};
+
+export default TodoList;
